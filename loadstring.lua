cfa60c9bba3182e2c4f3b9c06bb7ec483e24d985
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-05-10 05:59:15 -0400

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/Not-Kyle/Haunted.7z/main/src/Haunted.7z'),true))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/Not-Kyle/Haunted.7z/main/src/Haunted.7z.lua'),true))()
